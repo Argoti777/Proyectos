43e32ca9a87a71254cfaa0dc4a420e44f8c6ae70
Jualcry12 <alexis.velasco1203@hotmail.com>
2025-06-10 13:06:18 -0500

    avances_alexis_cambio de css en uno solo

--- principal/urls.py
+++ principal/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('campeonato/', views.campeonato, name='campeonato'),
     path('ayuda/', views.ayuda, name='ayuda'),
     path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
+    path('detalle_partido/', views.detalle_partido, name='detalle_partido'),
+    path('Estadisticas_campeonato/', views.Estadisticas_campeonato, name='Estadisticas_campeonato'),
 ]
 
--- principal/views.py
+++ principal/views.py
@@ -13,4 +13,12 @@ def ayuda(request):
 def inicio_sesion(request):
     return render(request, 'principal/inicio_sesion.html')  
 
+def detalle_partido(request):
+    return render(request, 'principal/detalle_partido.html')
+
+def estadisticas_partido(request):
+    return render(request, 'principal/estadisticas_partido.html')
+
+def Estadisticas_campeonato(request):
+    return render(request, 'principal/Estadisticas_campeonato.html')
 # Create your views here.
